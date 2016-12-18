dd232264184f1c8b9b7124d5d531a3ad8222db98
Michael Pratt <michael@pratt.im>
2016-12-18 12:31:44 -0800

    Read from stdin

--- gui/visualize.py
+++ gui/visualize.py
@@ -1,4 +1,7 @@
-import sys, pygame
+import fileinput
+import pygame
+import sys
+
 pygame.init()
 
 size = width, height = 500, 10
@@ -6,17 +9,25 @@ black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
 def redraw(leds):
     screen.fill(black)
 
     for i, led in enumerate(leds):
+        led = tuple([int(c) for c in led])
         pygame.draw.rect(screen, led, (10*i, 0, 10, 10))
 
-while 1:
+for line in fileinput.input():
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-    leds = [(255, 255, 255), (255, 0, 0), (0, 255, 0), (0, 0, 255)]
+    channels = line.split(',')
+    leds = chunks(channels, 3)
 
     redraw(leds)
     pygame.display.flip()
